f2524733ed360011824419a7b322872ab96f17f2
ari <10598811+CatWithAWand@users.noreply.github.com>
2022-09-13 18:58:21 +0300

    Release 1.0.4 (#4)
    * refactor(global): some code refactoring, added default export, docs updated by gh actions only now
    
    * chore(husky and gh-actions): fixed husky pre-commit to build ts, and changed run commit to print
    
    * ci(update-docs action): downgraded checkout action used to v2
    
    * ci(update-docs action): removed names and added additional dash '-' to last uses
    
    * chore(package-lock): updated package-lock, added npm command to update package-lock for update-docs
    
    * ci(ci running tests): disabled tests at ci for now due to possible jest issue
    
    * ci(all actions): removed package-lock update from ci, added it to pre-commit
    
    Sadly yarn users will have to run `yarn install` everytime after committing
    
    * ci(update-docs action): changed to token with workflows permissions
    
    * ci(update-docs action): added git add . command to include files
    
    The github push action has that command ommited from their example...
    
    * Updated documentation
    
    * ci(update-docs action): change on triggers to update docs only at release creation
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/Fragments/RandomFragment.ts
+++ src/Fragments/RandomFragment.ts
@@ -13,7 +13,7 @@ export default class RandomFragment extends FragmentBase {
    * no greater than `2 ** bits - 1`.
    *
    * @param bits - The number of bits for the fragment.
-   * @param func - Optional custom random function.
+   * @param fn - Optional custom random function.
    *
    * @throws `[RND_FUNCTION_RETURN_TYPE]` If custom function does not return number or bigint.
    */
@@ -27,14 +27,26 @@ export default class RandomFragment extends FragmentBase {
   }
 
   getValue(): bigint {
-    if (this.fn) return BigInt(this.fn());
+    if (this.fn) {
+      const rndNum = BigInt(this.fn());
+
+      if (rndNum >= BigInt(1 << this.bits))
+        throw new TypeError(
+          `[RND_FUNCTION_BAD_RETURN]: RandomFragment custom function returned a value bigger than 2 ** ${this.bits} - 1.`,
+        );
+
+      return rndNum;
+    }
+
+    if (this.bits <= 48) return BigInt(randomInt(1, 1 << 48)) - BigInt(1);
+
     // 48 bit parts due to randomInt range limitation
-    // and max param limit of Number.MAX_SAFE_INTEGER
+    // max param limit of Number.MAX_SAFE_INTEGER
     let rndNum = BigInt(1);
     for (let i = 1; i < this.bits / 48; i += 1) {
-      rndNum *= BigInt(randomInt(1, 2 ** 48));
+      rndNum *= BigInt(randomInt(1, 1 << 48));
     }
-    rndNum *= BigInt(randomInt(1, 2 ** (this.bits % 48)));
+    rndNum *= BigInt(randomInt(1, 1 << this.bits % 48));
 
     return rndNum - BigInt(1);
   }
