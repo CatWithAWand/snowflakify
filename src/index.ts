47be53db930d63599731b8e7759850caf7032b47
George <george@fozzie.dev>
2026-01-27 11:05:03 +0000

    feat(fragments): Add FragmentBase and type exports (#13)
    * Add FragmentBase and type exports
    
    * Change to type export

--- src/index.ts
+++ src/index.ts
@@ -13,3 +13,5 @@ export { default as ProcessFragment } from './Fragments/ProcessFragment.js';
 export { default as NetworkFragment } from './Fragments/NetworkFragment.js';
 export { default as RandomFragment } from './Fragments/RandomFragment.js';
 export { default as SequenceFragment } from './Fragments/SequenceFragment.js';
+export { default as FragmentBase } from './FragmentBase.js';
+export type * from './@types/index.js';
